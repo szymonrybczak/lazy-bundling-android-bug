07a47b72606f3e2261bda7cc05f669d62d74e34f
szymonrybczak <szymon.rybczak@gmail.com>
2023-07-17 17:21:29 +0200

    added lazy bundling example

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {Suspense} from 'react';
+
+const LazyText = React.lazy(() => import('./LazyText'));
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <Suspense
+      fallback={
+        <View style={s.container}>
+          <Text>Loading...</Text>
+        </View>
+      }>
+      <LazyText />
+    </Suspense>
+  );
+};
 
-export default App
+export default App;
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
